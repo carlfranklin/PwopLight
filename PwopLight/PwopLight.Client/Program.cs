9d0902060bfb6ebab7f679394688d8ca436c11cd
Carl Franklin <carl@appvnext.com>
2024-02-21 13:47:25 -0500

    Add project files.

---
+++ PwopLight/PwopLight.Client/Program.cs
@@ -0,0 +1,7 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
